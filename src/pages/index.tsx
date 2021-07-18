34ab8616eb7c407f52fb21dafb94da018251e35e
Nguyen Thanh Tuan <nttuancntt4@gmail.com>
2021-07-18 15:13:13 +0700

    Init

---
+++ src/pages/index.tsx
@@ -0,0 +1,62 @@
+import { Stream } from '@cloudflare/stream-react';
+import {
+  Button,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Container
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { MdMenu } from 'react-icons/md';
+
+import styles from './index.module.css';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  },
+  stream: {
+    borderRadius: '40px'
+  }
+}));
+export default function Home() {
+  const classes = useStyles();
+  return (
+    <>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              className={classes.menuButton}
+            >
+              <MdMenu />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Home
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <br />
+      <Container maxWidth="sm">
+        <Stream
+          className={styles.stream_component}
+          controls
+          src="d7a2ffcf3c28d6b583d95f2c8efdf210"
+          responsive
+        />
+      </Container>
+    </>
+  );
+}
